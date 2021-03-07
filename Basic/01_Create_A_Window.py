6bb8c2923d66c78f62042446d333f9b54fe6918c
FahimFBA <fahimbinamin@gmail.com>
2021-03-08 03:03:41 +0600

    1 of the basic

---
+++ Basic/01_Create_A_Window.py
@@ -0,0 +1,12 @@
+from tkinter import * # invoking the library
+# * means all
+
+window = Tk() # connecting window to the library
+
+# Tk is the shortform of Tkinter
+
+window.geometry('500x500+150+300')
+# ('width*height+position of the window')
+window.title('Hello Tkinter')
+#providing the title of the window
+window.mainloop() # allows to draw everything in the mainloop
